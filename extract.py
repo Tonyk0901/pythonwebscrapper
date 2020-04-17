30cb2fb708c370c17ef220b27115ab74b294b296
TonyK <sunghk0901@gmail.com>
2020-04-16 23:04:57 -0700

    Job title and location extraction function implemented

--- extract.py
+++ extract.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python+developer&l=Seattle,+WA&sort=date&limit={LIMIT}&radius=25"
+# https://www.indeed.com/jobs?q=python+developer&l=Seattle,+WA&sort=date&limit=50&radius=25
 
 
 def max_page_num():
@@ -17,6 +18,18 @@ def max_page_num():
 
 def extract_indeed_jobs(last_page_num):
     job = []
+    job_list = []
+    print("Extracting on process...")
     for p in range(last_page_num):
-        response = requests.get(f"{URL}&start={p*LIMIT}").status_code
-        print(response)
+        response = requests.get(f"{URL}&start={p*LIMIT}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        lists = soup.find_all("div", class_={"jobsearch-SerpJobCard"})
+        for l in lists:
+            try:
+                title = l.find("a")["title"]
+                location = l.find(class_={"recJobLoc"})["data-rc-loc"]
+                job_list.append({"title": title, "location": location})
+            except Exception:
+                pass
+    print("Extracting done! Result:\n")
+    print(job_list)
